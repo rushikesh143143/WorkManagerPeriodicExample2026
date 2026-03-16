66fbf1363d302dc64249bbf19d217033321ca324
Rushikesh Thak <rthak.rt@gmail.com>
2026-03-16 18:56:50 +0530

    1. Added View Binding

--- app/src/main/java/com/example/quotesapp2026/MainActivity.kt
+++ app/src/main/java/com/example/quotesapp2026/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quotesapp2026.adapter.QuotesAdapter
+import com.example.quotesapp2026.databinding.ActivityMainBinding
 import com.example.quotesapp2026.viewmodels.MainViewModel
 import com.example.quotesapp2026.viewmodels.MainViewModelFactory
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -21,23 +22,31 @@ class MainActivity : AppCompatActivity() {
     lateinit var mainViewModel: MainViewModel
     lateinit var adapter: QuotesAdapter
 
+    //view binding object step 1
+    private var _binding : ActivityMainBinding ? = null
+    private  val binding get() = _binding!!
+
+
+
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //viewbinding step 2
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 101)
         }
 
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        val recyclerView = findViewById<RecyclerView>(R.id.quotesRecyclerView)
 
+        binding.progressBar.visibility = View.VISIBLE
 
-        progressBar.visibility = View.VISIBLE
         adapter = QuotesAdapter()
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
+        binding.quotesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.quotesRecyclerView.adapter = adapter
 
 
        val quotesRepository = (application as QuoteApplication).quotesRepository
@@ -47,12 +56,17 @@ class MainActivity : AppCompatActivity() {
        mainViewModel.quotes.observe(this, Observer{
            Log.d("Quotes", it.toString())
            adapter.setQuotes(it)
-           progressBar.visibility = View.GONE
+           binding.progressBar.visibility = View.GONE
        })
 
        mainViewModel.startQuotesNotificationBackground()
 
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
